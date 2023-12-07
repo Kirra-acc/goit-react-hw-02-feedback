3e92998995a0ba623adb445aa7d371b67be90d12
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-12-07 17:57:05 +0200

    task 1 steps all 4 and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { Section } from './Section/Section.jsx';
+import { Notification } from './Notification/Notification.jsx';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFb = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
+  countTotalFeedback = (good, neutral, bad) => {
+    return good + neutral + bad;
+  };
+
+  render() {
+    const totalFb = this.countTotalFeedback;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          flexDirection: 'column',
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions addFb={this.addFb} />
+        </Section>
+
+        {totalFb(this.state.good, this.state.neutral, this.state.bad) > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              statData={this.state}
+              countTotalFeedback={this.countTotalFeedback}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from './Statistics.module.css';
+
+export class Statistics extends React.Component {
+  countPositiveFeedbackPercentage = (good, neutral, bad) => {
+    const total = this.props.countTotalFeedback(good, neutral, bad);
+    return (total > 0 ? (good / total) * 100 : 0).toFixed(2);
+  };
+  render() {
+    const { statData, countTotalFeedback } = this.props;
+    const { good, neutral, bad } = statData;
+    return (
+      <>
+        <div className={s.statWrapper}>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {countTotalFeedback(good, neutral, bad)}</p>
+          <p>
+            Positive Feedback:{' '}
+            {this.countPositiveFeedbackPercentage(good, neutral, bad)}%
+          </p>
+        </div>
+      </>
+    );
+  }
+}
